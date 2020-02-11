556540c6f13bfe270aaf3e386bd023f38f9014ab
michelle <foleymichelle9@gmail.com>
2020-02-11 10:59:02 -0700

    Add show and character class, write tests to make sure they exist

--- lib/character.rb
+++ lib/character.rb
@@ -1,1 +1,10 @@
+class Character
 
+  attr_reader :name, :actor, :salary
+
+  def initialize(name)
+    @name = name.values[0]
+    @actor = name.values[1]
+    @salary = name.values[2]
+  end
+end
---
+++ lib/show.rb
@@ -0,0 +1,8 @@
+class Show
+
+  attr_reader :name
+require "pry"; binding.pry
+  def initialize(name)
+    @name = knight_rider.values[0]
+  end
+end
